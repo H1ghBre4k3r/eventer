b2704956fd2306ca5c02fc37f907be99230c1a87
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-12-25 02:15:50 +0100

    Add pocketbase as dependency

--- cmd/eventer/main.go
+++ cmd/eventer/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "github.com/H1ghBre4k3r/eventer/internal/backend"
+
 func main() {
-	println("Hello, World!")
+	backend.Start()
 }
---
+++ internal/backend/backend.go
@@ -0,0 +1,13 @@
+package backend
+
+import (
+	"log" 
+	"github.com/pocketbase/pocketbase"
+)
+
+func Start() {
+	app := pocketbase.New()
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
+}

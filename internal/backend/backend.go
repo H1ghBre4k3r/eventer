17491b1afcd034dc288d2a8d9e2c1318614ba51a
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-12-28 15:43:53 +0100

    Register public directory

--- internal/backend/backend.go
+++ internal/backend/backend.go
@@ -2,13 +2,28 @@ package backend
 
 import (
 	"log"
+	"os"
 
 	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/apis"
+	"github.com/pocketbase/pocketbase/core"
 )
 
 func Start() {
 	app := pocketbase.New()
+
+	setup(app)
+
 	if err := app.Start(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+// Setup Pocketbase, i.e., register all hooks etc.
+func setup(app *pocketbase.PocketBase) {
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		// serves static files from the provided public dir (if exists)
+		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("pb_public"), false))
+		return nil
+	})
 }
